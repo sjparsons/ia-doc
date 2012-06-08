5833c331f82b83e849119db6cc129622495a3bcd
Matt Buresh <mattburesh@gmail.com>
2012-06-08 14:59:55 -0500

    Added continuations and conditionals

--- iadoc.js
+++ iadoc.js
@@ -1,6 +1,11 @@
 	var flip = 0;
+	var collapse = '<span class="collapse">-</span>';
+	var boom = '<span class="boom">+</span>';
+
 	$('a').on('click', function(e)
 	{
 		e.preventDefault();
-		$(this).parent().children().children().slideToggle();
+		$(this).parent().children().children().toggle();
+
 	});
+
