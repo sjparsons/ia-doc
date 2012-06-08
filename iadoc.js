72434cf5bf6a92269186742f1d2c69d913d79205
Matt Buresh <mattburesh@gmail.com>
2012-06-08 11:05:33 -0500

    Layout change

--- iadoc.js
+++ iadoc.js
@@ -1,50 +1,6 @@
-function formatting()
-{
-	var windowWidth = $(window).width();
-	var numBranches = 1.0 / $('li.parent').length;
-
-	var width = (numBranches * windowWidth) - 20;
-	$('li.parent').css({
-		'width' : width,
-		'margin' : '1px'
-	});
-
-	$('li.page, li.file').not('.parent, .root').each(function(index)
+	var flip = 0;
+	$('a').on('click', function(e)
 	{
-		var $this = $(this);
-		var parentWidth = $this.parent().width();
-		var numChild = $this.parent().children().length;
-		var newWidth = ((1.0 / numChild) * parentWidth) - 5;
-
-		$this.css({
-			'width' : newWidth,
-			'margin-left' : '1px'
-		});
+		e.preventDefault();
+		$(this).parent().children().children().slideToggle();
 	});
-}
-
-function drawLines()
-{
-
-}
-
-function createLine(x1,y1, x2,y2)
-{
-	var length = Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
-    var angle  = Math.atan2(y2 - y1, x2 - x1) * 180 / Math.PI;
-    var transform = 'rotate('+angle+'deg)';
-    
-		var line = $('<div>')
-			.appendTo('#container')
-			.addClass('line')
-			.css({
-			  'position': 'absolute',
-			  '-webkit-transform':  transform,
-			  '-moz-transform':     transform,
-			  'transform':          transform
-		})
-	.width(length)
-	.offset({left: x1, top: y1});
-
-	return line;
-}
