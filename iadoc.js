517e85268e0a0e7dafaead88a1c7899dc1b46d0f
Matt Buresh <mattburesh@gmail.com>
2012-06-11 10:56:58 -0500

    Removed Areas and conditionals, added level collapsing

--- iadoc.js
+++ iadoc.js
@@ -1,12 +1,35 @@
-	var flip = 0;
-	var collapse = '<span class="collapse">-</span>';
-	var boom = '<span class="boom">+</span>';
+	var collapse = '<span class="collapse">[-/+]</span>';
+	collapsable();
+	
+	function collapsable()
+	{
+		$('a').each(function(index)
+		{
+			if( $(this).parent().find('li').length > 0 )
+			{
+				$(this).before(collapse);
+			} 
+		});
+	}
 
 	$('a').on('click', function(e)
 	{
-		e.preventDefault();
-		$(this).parent().children().children().toggle();
-
-
+		var $this = $(this);
+		if( $this.attr('href') == "" || $this.attr('href') == 0 )
+		{
+			e.preventDefault();	
+		}
+		//May remove this, not sure if necessary.
+		
+		$this.parent().children().children().toggle();
 	});
 
+	$('span.collapse').on('click', function()
+	{
+		var $this = $(this);
+
+		$this.parent().parent().siblings().children().children().children().toggle();
+		$this.parent().siblings().children().children().toggle();
+		$this.parent().children().children().toggle();
+
+	});
