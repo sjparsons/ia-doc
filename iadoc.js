7fa04596a53941fb3e14189c7ef64557714fb2dd
sjparsons <sjparsons@gmail.com>
2012-06-29 10:33:06 -0500

    testing some ideas

--- iadoc.js
+++ iadoc.js
@@ -3,7 +3,13 @@
 		// Children Toggling
 		$('.iadoc li').click(function(){
 			// hide children li's wheneve the current li is clicked.
-			$(this).children().children('li').toggle();
+			li = $(this)
+			descendent_lists = li.children('ol,ul');
+			if (descendent_lists.length) {
+				descendent_lists.toggle();
+				li.toggleClass('collapsed');
+			}
+			
 			return false; // to stop the event bubbling up.
 		});
 		
@@ -15,23 +21,27 @@
 			}
 		});
 		
-		// Level Toggling
-		$('.iadoc li').prepend('<button class="toggle" name="level_toggle" value="">-</button>');
+		// Continuation 
 		
-		$('button.toggle').show().click(function(){
-			var show = $(this).val();												// hide or show?	
-			var cur_level_lis = $(this).parent().parent().children('li'); 			// get all li's at level above.
-			if (!show) {
-				cur_level_lis.children().children('li').hide(); 					// hide children of the current level
-				cur_level_lis.children('button.toggle').val('1').text('+');			// make button 'show' children next time.
-			}
-			else {
-				cur_level_lis.children().children('li').show();						// hide children of the current level
-				cur_level_lis.children('button.toggle').val('').text('-');			// reset button to 'hide' children next time.
-			}
-			return false;
-		});
+		$('li.continuation').prepend('[ ').append(' ]');
 		
+		// // Level Toggling
+		// $('.iadoc li').prepend('<button class="toggle" name="level_toggle" value="">-</button>');
+		// 
+		// $('button.toggle').show().click(function(){
+		// 	var show = $(this).val();												// hide or show?	
+		// 	var cur_level_lis = $(this).parent().parent().children('li'); 			// get all li's at level above.
+		// 	if (!show) {
+		// 		cur_level_lis.children().children('li').hide(); 					// hide children of the current level
+		// 		cur_level_lis.children('button.toggle').val('1').text('+');			// make button 'show' children next time.
+		// 	}
+		// 	else {
+		// 		cur_level_lis.children().children('li').show();						// hide children of the current level
+		// 		cur_level_lis.children('button.toggle').val('').text('-');			// reset button to 'hide' children next time.
+		// 	}
+		// 	return false;
+		// });
+		// 
 		/*
 		$('li').hover(
 			function(){ // Hover In
