597303b74b8a16e9535207c9681911b560d9bdfa
Matt Buresh <mattburesh@gmail.com>
2012-07-02 15:16:56 -0500

    Added programatic level toggling

--- iadoc.js
+++ iadoc.js
@@ -1,25 +1,38 @@
 	(function($){
 
+		//Programatic level toggling
+		if( $('.iadoc>ol>li').length > 0 ){
+			$('<p class="ltoggle">Toggle: <a href="#" class="collapse_one">Level 1</a> | </p>').insertAfter('.root');
+		}
+		if( $('.iadoc>ol>li>ol>li').length > 0 ){
+			$('<a href="#" class="collapse_two">Level 2</a>').appendTo('.ltoggle');
+		}
+		if( $('.iadoc>ol>li>ol>li>ol>li').length > 0 ){
+			$('.ltoggle').append(' | ');
+			$('<a href="#" class="collapse_three">Level 3</a>').appendTo('.ltoggle');
+		}
+
 		// Children Toggling
 		$('.iadoc li').click(function(){
 			// hide children li's wheneve the current li is clicked.
 			$(this).children().children('li').toggle();
 			return false; // to stop the event bubbling up.
 		});
-		
+
 		//Make "page" the default class for list items
 		$('li').each(function(index)
 		{
-			if( !$(this).attr('class')) {
-				$(this).addClass('page');
+			var $this = $(this);
+			if( !$this.attr('class')) {
+				$this.addClass('page');
 			}
 		});
-		
+
 		// Level Toggling
-		$('.iadoc li').prepend('<button class="toggle" name="level_toggle" value="">-</button>');
-		
+		/*$('.iadoc li').prepend('<button class="toggle" name="level_toggle" value="">-</button>');
+
 		$('button.toggle').show().click(function(){
-			var show = $(this).val();												// hide or show?	
+			var show = $(this).val();												// hide or show?
 			var cur_level_lis = $(this).parent().parent().children('li'); 			// get all li's at level above.
 			if (!show) {
 				cur_level_lis.children().children('li').hide(); 					// hide children of the current level
@@ -30,18 +43,22 @@
 				cur_level_lis.children('button.toggle').val('').text('-');			// reset button to 'hide' children next time.
 			}
 			return false;
+		});*/
+
+		$('.collapse_one').click(function(){
+			$('.iadoc>ol>li').click();
+			return false
 		});
-		
-		/*
-		$('li').hover(
-			function(){ // Hover In
-				$(this).children('button.toggle').show();
-			},
-			function(){  // Hover Out
-				$(this).children('button.toggle').hide();
-			}
-		);
-		*/
-		
-	})(jQuery); 
-	
+
+		$('.collapse_two').click(function(){
+			$('.iadoc>ol>li>ol>li').click();
+			return false
+		});
+
+		$('.collapse_three').click(function(){
+			$('.iadoc>ol>li>ol>li>ol>li').click();
+			return false
+		});
+
+	})(jQuery);
+
