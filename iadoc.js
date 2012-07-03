31f16c6c34a9799c3bc92efc9610838e707ff86a
Matt Buresh <mattburesh@gmail.com>
2012-07-03 14:45:48 -0500

    Removed old comments

--- iadoc.js
+++ iadoc.js
@@ -28,23 +28,6 @@
 			}
 		});
 
-		// Level Toggling
-		/*$('.iadoc li').prepend('<button class="toggle" name="level_toggle" value="">-</button>');
-
-		$('button.toggle').show().click(function(){
-			var show = $(this).val();												// hide or show?
-			var cur_level_lis = $(this).parent().parent().children('li'); 			// get all li's at level above.
-			if (!show) {
-				cur_level_lis.children().children('li').hide(); 					// hide children of the current level
-				cur_level_lis.children('button.toggle').val('1').text('+');			// make button 'show' children next time.
-			}
-			else {
-				cur_level_lis.children().children('li').show();						// hide children of the current level
-				cur_level_lis.children('button.toggle').val('').text('-');			// reset button to 'hide' children next time.
-			}
-			return false;
-		});*/
-
 		$('.collapse_one').click(function(){
 			$('.iadoc>ol>li').click();
 			return false
